09012ac7b6719de41ef89e870284b2ce94990377
abubakkarsiddik123 <abu089604@gmail.com>
2026-06-23 03:36:19 +0600

    add responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="border-b px-2">
+      <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
+        <div className="flex gap-2 items-center">
+          <Image
+            src={"/navlogo.png"}
+            alt="logo"
+            loading="eager"
+            width={40}
+            height={40}
+            className="object-cover h-auto w-auto"
+          />
         </div>
-    );
+
+        <ul className="flex items-center gap-5 text-sm">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/all-photos"}>Products</Link>
+          </li>
+          <li>
+            <Link href={"/profile"}> My Profile</Link>
+          </li>
+        </ul>
+
+        <div className="flex gap-4">
+          <ul className="flex items-center  text-sm">
+            <li>
+              <Link href={"/signup"}>SignUp</Link>
+            </li>
+            <li>
+              <Link href={"/signin"}>SignIn</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
