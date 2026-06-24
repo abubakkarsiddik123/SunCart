6103f8dbc9dbec13f4f7858a36080f3dbf73f7e6
abubakkarsiddik123 <abu089604@gmail.com>
2026-06-24 12:37:23 +0600

    update env

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://sun-cart-m4ty7q3so-abu-bakkar.vercel.app/"
+    baseURL: "https://sun-cart-phi.vercel.app"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
