ef9d9073965d12b4cd4e5b0dfd7ca0003a641e37
abubakkarsiddik123 <abu089604@gmail.com>
2026-07-08 14:13:10 +0600

    update

--- src/app/loading.jsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "@heroui/react";
-
-export default function Loading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <Spinner size="lg" label="Loading..." />
-    </div>
-  );
-}
